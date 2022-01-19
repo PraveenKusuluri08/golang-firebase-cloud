7862efca83f9c4ce3ddea622d84faba790592a90
Praveenkusuluri08 <praveenkusuluri08@gmail.com>
2022-01-19 22:18:31 +0530

    Create post and the users directory and file for closed end file added

--- Services/Posts/controller.go
+++ Services/Posts/controller.go
@@ -1,1 +1,45 @@
 package Posts
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/PraveenKusuluri08/utils"
+)
+
+func createPost(post Posts) string {
+
+	//TODO:add this route as the closed end to get the logged in userData
+	//TODO:From the token by that we can store user data into the db
+
+	//TODO:Add validators to the Post by that we can prevent the empty data
+	app := utils.InitializeFbApp()
+	post.CreatedAt = time.Now()
+
+	db, _ := app.Firestore(context.Background())
+
+	_, set, err := db.Collection("POSTS").Add(context.Background(), post)
+
+	fmt.Println(set)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		return "Document added successfully"
+	}
+	return ""
+}
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Access", "POST")
+	var post Posts
+	_ = json.NewDecoder(r.Body).Decode(&post)
+
+	if msg := createPost(post); msg != "" {
+		json.NewEncoder(w).Encode(msg)
+	}
+}
